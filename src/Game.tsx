8e5f8e83badd619ec3853568cb9d426649ef9da0
sbaudray <sebastien.baudray@hetic.net>
2019-07-11 20:27:26 +0200

    rumble in the jungle

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,33 +1,149 @@
 import React from "react";
 import "./Game.css";
 
-const board: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-interface CellProps {
-  mark?: number;
+enum PlayerT {
+  Cross = "Cross",
+  Circle = "Circle"
 }
 
-function Cell({ mark }: CellProps) {
-  return <div>{mark}</div>;
+enum StatusT {
+  Running = "Running",
+  Draw = "Draw",
+  Won = "Won"
 }
 
-interface BoardProps {
-  board: typeof board;
+type MarkT = null | PlayerT;
+
+type BoardT = MarkT[];
+
+interface GameStateT {
+  board: BoardT;
+  player: PlayerT;
+  status: StatusT;
 }
 
-function Board({ board }: BoardProps) {
+type GameActionT = { type: "PLAY"; square: number };
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+];
+
+function gameStateReducer(state: GameStateT, action: GameActionT) {
+  const { type, square } = action;
+  const { board, player } = state;
+
+  switch (type) {
+    case "PLAY": {
+      // dont do that, it breaks shallow comparisons
+      board[square] = player;
+
+      let winner = null;
+
+      for (const [x, y, z] of winningCombos) {
+        if (board[x] && board[x] === board[y] && board[y] === board[z]) {
+          winner = board[x];
+        }
+      }
+
+      if (winner) return { ...state, status: StatusT.Won };
+
+      if (board.every(Boolean)) return { ...state, status: StatusT.Draw };
+
+      return {
+        ...state,
+        player: player === PlayerT.Circle ? PlayerT.Cross : PlayerT.Circle
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+function gameStateToString(state: GameStateT) {
+  const { status, player } = state;
+
+  if (status === StatusT.Draw) return "It's a draw !";
+
+  if (status === StatusT.Won) return `Won: ${player}`;
+
+  return `Playing: ${player}`;
+}
+
+function markToString(mark: MarkT) {
+  switch (mark) {
+    case PlayerT.Cross:
+      return "X";
+    case PlayerT.Circle:
+      return "O";
+    case null:
+    default:
+      return "";
+  }
+}
+
+interface SquareProps {
+  mark: MarkT;
+  onClick?: () => void;
+}
+
+function Square({ mark, onClick }: SquareProps) {
   return (
-    <div>
-      {board.map(item => (
-        <Cell key={item} mark={item} />
-      ))}
+    <div className="square" onClick={onClick}>
+      {markToString(mark)}
     </div>
   );
 }
 
-const Game: React.FC = () => {
-  return <Board board={board} />;
-};
+interface BoardProps {
+  children: React.ReactNode;
+}
+
+function Board({ children }: BoardProps) {
+  return <div className="board">{children}</div>;
+}
+
+interface GameStatusProps {
+  state: GameStateT;
+}
+
+function GameStatus({ state }: GameStatusProps) {
+  return <div className="gameStatus">{gameStateToString(state)}</div>;
+}
+
+function Game() {
+  const [gameState, dispatch] = React.useReducer(gameStateReducer, {
+    board: Array(9).fill(null),
+    player: PlayerT.Circle,
+    status: StatusT.Running
+  });
+
+  const { board, status } = gameState;
+
+  const squares = board.map((mark, index) => (
+    <Square
+      key={index}
+      mark={mark}
+      onClick={
+        status !== StatusT.Running || mark
+          ? undefined
+          : () => dispatch({ type: "PLAY", square: index })
+      }
+    />
+  ));
+
+  return (
+    <>
+      <GameStatus state={gameState} />
+      <Board>{squares}</Board>
+    </>
+  );
+}
 
 export default Game;
-
